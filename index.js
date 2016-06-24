a8f97820af55ae14ed3ca922c79fb5db8897c257
Cole Turner <turner.cole@gmail.com>
2016-06-24 00:45:44 -0400

    More flexible query options.
    The query can be either a string or an object. If it's a string, it will append to the source. 
    
    If it's an object, you can specify both `append` and `prepend` for your every whim.

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 module.exports = function(source, map) {
   this.cacheable();
+  
+  var query = this.query;
+  
+  if(typeof query == 'string')
+  {
+    query = { 'append' : query };
+  }
+  
+  if(typeof query.prepend != 'undefined' && typeof query.prepend == 'string')
+  {
+    query.prepend = [query.prepend];
+  }
 
-  var query = this.query.substring(1) + "\n"
-
-  source = query + source
+  if(typeof query.append != 'undefined' && typeof query.append == 'string')
+  {
+    query.append = [query.append];
+  }
+  
+  if(typeof query.prepend != 'undefined')
+  {
+    query.prepend.forEach(function(str)
+    {
+      source = str + source;
+    });
+  }
+  
+  if(typeof query.append != 'undefined')
+  {
+    query.append.forEach(function(str)
+    {
+      source += str
+    });
+  }
 
   this.callback(null, source, map)
 };
